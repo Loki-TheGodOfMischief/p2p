37b1c68b3fb9c2bc6974e9720d962e917b1303ff
Loki-TheGodOfMischief <Mantis7xOfficial@gmail.com>
2025-07-25 19:58:56 +0530

    salt decoded

--- src/server/PrintAllUsers.java
+++ src/server/PrintAllUsers.java
@@ -2,6 +2,7 @@ package server;
 
 import java.io.*;
 import java.util.*;
+import java.util.Base64;
 
 public class PrintAllUsers {
     public static void main(String[] args) {
@@ -14,7 +15,21 @@ public class PrintAllUsers {
                 System.out.println("Active: " + user.isActive());
                 System.out.println("Created: " + user.getCreatedDate());
                 System.out.println("Last Login: " + user.getLastLoginDate());
-                System.out.println("Salt: " + user.getSalt());
+
+                String saltBase64 = user.getSalt();
+                System.out.println("Salt (Base64): " + saltBase64);
+
+                // Decode Base64 to byte array
+                byte[] decodedSalt = Base64.getDecoder().decode(saltBase64);
+
+                // Convert bytes to hex string
+                StringBuilder hex = new StringBuilder();
+                for (byte b : decodedSalt) {
+                    hex.append(String.format("%02x ", b));
+                }
+
+                System.out.println("Salt (Decoded Bytes): " + hex.toString().trim());
+
                 System.out.println("Hashed Password: " + user.getHashedPassword());
                 System.out.println("-----");
             }
